877adb2f2728be4a2688baf973cb48393a0fbdad
Tafé <79672264+zetafe1@users.noreply.github.com>
2023-03-10 15:29:00 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,6 +6,8 @@ lua54 "yes"
 
 this_is_a_map "yes"
 
+author "YourMAPS"
+
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 
 -- escrow_ignore {
@@ -13,3 +15,9 @@ rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aw
 -- }
 
 -- dependency '/assetpacks'
+
+------ 
+
+-- This map is DCMA copywrited - do not change textures & use it in ANY way for comercial purposes!
+-- Respect the authors!
+-- https://discord.gg/4FegSMTS
